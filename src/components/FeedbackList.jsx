2127dc0b2cd4b8eb934003a46f4c530c493bc820
Rahul Kumar <rahulkumarkvbr@gmail.com>
2023-05-01 00:35:29 +0530

    rating component added

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,15 +1,33 @@
 import FeedBackItem from "./FeedBackItem"
+import {motion, AnimatePresence} from 'framer-motion'
 
 function FeedbackList({feedback, handleDelete}) {
     if(!feedback || feedback.length === 0){
         return <p>No Feedback Yet</p>
     }
     console.log(feedback)
-  return (
+//   return (
+//     <div className='feedbacl-list'>
+//         {feedback.map((item) => (
+//             <FeedBackItem key={item.id} item={item} handleDelete = {handleDelete}/>
+//         ))}
+//     </div>
+//   )
+
+return (
     <div className='feedbacl-list'>
+        <AnimatePresence>
         {feedback.map((item) => (
+            <motion.div
+             key={item.id}
+             initial={{opacity: 0}}
+             animate={{opacity: 1}}
+             exit={{opacity: 0}}
+             >
             <FeedBackItem key={item.id} item={item} handleDelete = {handleDelete}/>
+        </motion.div>
         ))}
+        </AnimatePresence>
     </div>
   )
 }
